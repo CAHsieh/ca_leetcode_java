cddee116fa6f9d5b2435316905d0f450f423c72d
CABED <chianhsieh511@gmail.com>
2017-10-07 02:51:46 +0800

    437 accepted

--- 437.java
+++ 437.java
@@ -11,10 +11,7 @@ class Solution {
     public int pathSum(TreeNode root, int sum) {
         if (root == null)
             return 0;
-        int ans = 0;
-        if (root.val <= sum) {
-            ans = isMatch(root, sum);
-        }
+        int ans = isMatch(root, sum);
         if (root.left != null)
             ans += pathSum(root.left, sum);
         if (root.right != null)
@@ -25,10 +22,8 @@ class Solution {
     private int isMatch(TreeNode node, int sum) {
         if (node == null)
             return 0;
-        if (sum > node.val) {
-            return 0;
-        } else if (sum == node.val) {
-            return 1;
+        if (sum == node.val) {
+            return 1 + (isMatch(node.left, sum - node.val) + isMatch(node.right, sum - node.val));
         } else {
             return (isMatch(node.left, sum - node.val) + isMatch(node.right, sum - node.val));
         }
